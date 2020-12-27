7984c6d42a6e91d2a29f4a43540359a9fccc5b17
tanjased <71439082+tanjased@users.noreply.github.com>
2020-12-27 21:32:24 +0100

    steps

--- 04-Milestone Project - 1/02-Milestone Project - 1.py
+++ 04-Milestone Project - 1/02-Milestone Project - 1.py
@@ -1,37 +1,51 @@
-def display_list(lst):
-    return(f'{lst[:3]}\n{lst[3:6]}\n{lst[6:]}')
+def display_board():
+    lst = ['X','O','X','O','X','O','X','O','X']
+    print(*lst)
 
-lst = display_list([' ']*9)
+    # return f'{lst[:3]}\n{lst[3:6]}\n{lst[6:]}'
+
+
+
+lst = display_board()
+# lst[2]=2
+print(type(lst))
+
 
 def user1_pos():
     choice = 'wrong'
-    while choice.isdigit() == False or int(choice) not in range(0,10):
+    while choice.isdigit() == False or int(choice) not in range(0, 10):
         choice = input("Choose the position: ")
         if choice.isdigit() == False:
             print('Please, choose a number')
-        elif int(choice) not in range(0,10):
+        elif int(choice) not in range(0, 10):
             print('Your choice is out of range')
-    return(int(choice))
+    return int(choice)
+
+
 # user1_pos()
 
 def user2_pos():
     choice = 'wrong'
-    while choice.isdigit() == False or int(choice) not in range(0,10):
+    while choice.isdigit() == False or int(choice) not in range(0, 10):
         choice = input("Choose the position: ")
         if choice.isdigit() == False:
             print('Please, choose a number')
-        elif int(choice) not in range(0,10):
+        elif int(choice) not in range(0, 10):
             print('Your choice is out of range')
-    return(int(choice))
-# user2_pos()
+    return int(choice)
 
-def user1_placement(lst,position):
-    value = input('Make your move: ')
-    while False:
-        if value != 'x' or value != 'o':
-            print('Put x or o please')
 
-    lst[position] = value
-    return(lst)
-user1_placement(lst,2)
+# pos2 = user2_pos()
 
+
+def user_placement(board, position):
+    # value = 'input'
+    value = 1
+    rng = ['x', 'o']
+    while value not in rng:
+        value = input('Make your move: ')
+        print('Put x or o please')
+    board[position] = value
+    print(board)
+
+# user_placement(lst, 2)
