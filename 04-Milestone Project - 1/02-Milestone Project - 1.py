961015790d64d2da94a76a6b381d6a90e89545cf
tanjased <71439082+tanjased@users.noreply.github.com>
2020-12-28 20:35:13 +0100

    try

--- 04-Milestone Project - 1/02-Milestone Project - 1.py
+++ 04-Milestone Project - 1/02-Milestone Project - 1.py
@@ -1,14 +1,17 @@
 ## First step is to display the game board
 def display_board():
-    # lst = ['']*9
-    lst = ['X','O','X','O','O','O','X','O','X']
-    # print(*lst)
+    lst = ['']*9
+    # lst = ['X','1','1','X','0','0','X','7','7']
+    print("Let's start the game!")
+    print("|".join(lst[:3]))
+    print("|".join(lst[3:6]))
+    print("|".join(lst[6:]))
     # print(f'{lst[:3]}\n{lst[3:6]}\n{lst[6:]}'
     return lst    # Had problem with displaying on a few lines
 
 
-lst = display_board()
-# print(lst)
+# lst = display_board()
+# print(lst[0])
 
 ## Define the player's index position
 def user1_pos():
@@ -50,8 +53,30 @@ def check_win(board,marker):
     elif all(el == marker for el in board[7:]):
         print('yay3')
 
-check_win(lst,'O')
+    # for i in len(board):
+    #     if board[i] == board[i+3]
+    elif board[0] == board[3] and board[0] == board[6]:
+        print('col1')
+    elif board[1] == board[4] and board[1] == board[7]:
+        print('col2')
+    elif board[2] == board[5] and board[2] == board[8]:
+        print('col3')
+    elif board[0] == board[4] and board[0] == board[8]:
+        print('diag1')
+    elif board[2] == board[4] and board[2] == board[6]:
+        print('diag2')
+    elif ' ' not in board:
+        print('tie')
+        return True
 
+# check_win(lst,'X')
+
+## Who goes first
+import random
+def choose_first():
+    flip = random.randint(1,2)
+    print(f'User {flip} goes first')
+# choose_first()
 
 ## Check if they want to continue.
 def gameon():
@@ -62,6 +87,18 @@ def gameon():
         print('Please choose Y or N. ')
     if answer == 'Y':
         return True
-    elif answer == 'N':
+    else:
         print('Ok. Come back later')
+        return False
 # gameon()
+
+
+game_on = True
+
+while game_on:
+    board = display_board()
+    choose_first()
+    pos = user1_pos()
+    marker = user_placement(board, pos)
+    check_win(board, marker)
+    gameon()
